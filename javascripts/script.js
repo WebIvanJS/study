dba5c130a9fc75dae868e9b49ba62129330f010f
WebIvanJS <vanya.vaschak@gmail.com>
2022-05-03 22:08:36 +0200

    first commit

---
+++ javascripts/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+
+let number =5;
+const lel = 1;
+
+console.log(lel);
---
+++ previev/script.js
@@ -0,0 +1,55 @@
+
+function myFirstApp (name, age) {
+    alert(`Прівєт, мене звати ${name} і це моя перша функція`);
+
+
+    function showSkils(q){
+        let skills = ['html', 'css', 'js'];
+        let i=0;
+        while (i < skills.length ){
+            console.log(skills[i]);
+            i++;
+        }
+        
+    }
+    showSkils(3);
+
+    function checkAge(){
+        if( age < '18') {
+            console.log(`Ще рано! Тільки ${age} ркоів`);
+        } else {
+            console.log('Вже норм');
+        }
+
+    }
+    checkAge();
+    function calcPow(num) {
+        return num * num;
+
+    }
+    console.log(calcPow(4));
+
+
+
+}
+
+myFirstApp('Ivan','14');
+
+const df = 4;
+let storeName = 'Lidl';
+const storeDescription = {
+    budget: 10000,
+    employees: ['Ivan', 'Alex', 'John'],
+    products: {
+        pizza: 10,
+        salame: 15
+    },
+    open: true
+};
+
+alert('Hello');
+
+const result = confirm ('Are you hear?');
+
+
+
