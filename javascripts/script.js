5e87aee3e84c47c9617f603647bb29152818adee
Ivan Vashchak <vanya.vaschak@gmail.com>
2022-05-04 22:04:29 +0200

    last final

--- javascripts/script.js
+++ javascripts/script.js
@@ -3,5 +3,24 @@
 
 let number =5;
 const lel = 1;
+const numberOfFilms = +prompt('Скільки фільмів ви бачили?','');
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 
-console.log(lel);
+};
+
+const a = prompt('Останній фільм?', ''),
+      b = prompt('На скільки оцінете?', ''),
+      c = prompt('Останній фільм?', ''),
+      d = prompt('На скільки оцінете?', '');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+
+console.log(personalMovieDb);
+13435t
