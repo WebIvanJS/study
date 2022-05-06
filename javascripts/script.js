1a3f4b2e00b7d282fcfebcb11ddb122c3ae8e6f5
Ivan Vashchak <vanya.vaschak@gmail.com>
2022-05-06 21:53:13 +0200

    les 23

--- javascripts/script.js
+++ javascripts/script.js
@@ -33,3 +33,20 @@ if (hamburger && cola || fries === 3 && nuggets) {
     console.log('non');
 }
 
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += "\n";
+
+}
+console.log(result);
+
+
