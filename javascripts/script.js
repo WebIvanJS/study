c59fcd3759664732c4253f6c60cc98f49221c60b
Ivan Vashchak <vanya.vaschak@gmail.com>
2022-05-08 13:48:00 +0200

    mm

--- javascripts/script.js
+++ javascripts/script.js
@@ -61,3 +61,92 @@ if (personalMovieDB.count < 10) {
 } else {
     console.log("Произошла ошибка");
 }
+// Место для первой задачи
+
+
+
+function sayHello(a) {
+    let res = 'Привет, '+ a+'!';
+    return res;
+
+}
+
+sayHello('aynjy');
+
+
+// Место для второй задачи
+function returnNeighboringNumbers(a) {
+    const s = [];
+    let j = -1;
+    for (let i = 0; i < 3; i++) {
+        let res = a + j;
+        s[i] = res;
+        j++;
+    }
+
+    return s;
+
+}
+returnNeighboringNumbers(8);
+
+
+// Место для третьей задачи
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+getMathResult(10, 5);
+console.log(getMathResult(10, 5));
+
+
+function sayHello(name) {
+    return `Привет, ${name}!`;
+}
+
+sayHello('Alex');
+
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num + 1];
+}
+
+returnNeighboringNumbers(5);
+
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+getMathResult(10, 5);
