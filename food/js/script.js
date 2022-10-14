11b7e682da479617b07debab6505121889b8e3d7
WebIvanJS <vanya.vaschak@gmail.com>
2022-10-14 19:56:49 +0200

    food

---
+++ food/js/script.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelectorAll('.tabheader__items');
+    
+    
+        function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    
+    }
+
+    hideTabContent();
+    showTabContent();
+   
+
+    tabsParent.addEventListener('click', (event) => {
+        console.log(tabsParent);
+        
+    });
+});
